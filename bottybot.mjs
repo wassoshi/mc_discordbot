2c7e42a422b7a174852537cd53e95220776bd0c4
wassoshi <wassoshi@gmail.com>
2024-09-13 09:43:56 +0300

    final

--- bottybot.mjs
+++ bottybot.mjs
@@ -469,7 +469,13 @@ function runSalesBot() {
             try {
                 const saleData = await fetchSaleDataFromOpenSea(sale.tokenId, sale.sellerAddress);
                 if (saleData) {
-                    const contractAddress = saleData.protocolAddress.toLowerCase();
+                    const contractAddress = saleData.nft?.contract?.toLowerCase();
+
+                    if (!contractAddress) {
+                        console.error('Contract address not found in sale data');
+                        continue;
+                    }
+
 
                     if (contractAddress === OLD_WRAPPER_CONTRACT_ADDRESS.toLowerCase()) {
                         await announceOldWrapperSale(
